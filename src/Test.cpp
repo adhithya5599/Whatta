642899a27f3c213d806d15a47a4adcaca4955041
adhithya5599 <adhithya5599@gmail.com>
2023-11-09 21:19:12 +0530

    initial commit

---
+++ src/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace Whatta {
+
+	void Print()
+	{
+		printf("Welcome to Whatta Engine!\n");
+	}
+}
---
+++ src/Test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace Whatta {
+	
+	_declspec(dllexport) void Print();
+}
