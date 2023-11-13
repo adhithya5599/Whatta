7df3b6a77c67647f67078608b4700ecd59cbcc7b
Adhithya Narayanan <65213873+adhithya5599@users.noreply.github.com>
2023-11-13 13:42:05 +0530

    added logging support (#2)
    Co-authored-by: adhithya5599 <adhithya5599@gmail.com>

--- Whatta/src/Whatta.h
+++ Whatta/src/Whatta.h
@@ -3,6 +3,7 @@
 //For client use
 #include <stdio.h>
 #include "Whatta/Application.h"
+#include "Whatta/Log.h"
 //-----------ENTRY POINT-----------
 #include "Whatta/EntryPoint.h"
 //---------------------------------
---
+++ Whatta/src/Whatta/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Whatta {
+
+	class WHATTA_API Log
+	{
+		public:
+			static void Init();
+
+			inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+			inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+		private:
+			static std::shared_ptr<spdlog::logger> s_CoreLogger;
+			static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// For Core
+#define WH_CORE_TRACE(...)		::Whatta::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define WH_CORE_INFO(...)		::Whatta::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define WH_CORE_WARN(...)		::Whatta::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define WH_CORE_ERROR(...)		::Whatta::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define WH_CORE_FATAL(...)		::Whatta::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+//For Client
+#define WH_TRACE(...)		::Whatta::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define WH_INFO(...)		::Whatta::Log::GetClientLogger()->info(__VA_ARGS__)
+#define WH_WARN(...)		::Whatta::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define WH_ERROR(...)		::Whatta::Log::GetClientLogger()->error(__VA_ARGS__)
+#define WH_FATAL(...)		::Whatta::Log::GetClientLogger()->fatal(__VA_ARGS__)
